58c29f26e5f23348139f00a7b87489736fda4090
GustavoM7 <gustavosmarquesf@gmail.com>
2019-12-06 23:59:16 -0300

    corrindo path do db

--- server/localStorage/db.js
+++ server/localStorage/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './database.db'
+    storage: './localStorage/database.db'
 });
 
 const db = {}
