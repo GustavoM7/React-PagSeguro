a447725b9851b6896e78fa80d195dd4656d0a4f7
GustavoM7 <gustavosmarquesf@gmail.com>
2019-12-14 16:40:45 -0300

    melhorando midleware de cep

--- server/routes/Correios.js
+++ server/routes/Correios.js
@@ -2,22 +2,37 @@ const axios = require("axios");
 
 module.exports = {
   AdressGeter(req, res){
-    const url = "http://viacep.com.br/ws/"+req.params.cep+"/json/";
-    console.log("Solicitando endereço para CEP: "+req.params.cep)
+    const cep = req.params.cep;
+    console.log("Solicitando endereço para CEP: "+cep);
+    const validaCep = /^[0-9]{8}$/;
 
-    axios({
-      method: 'get',
-      url: url,
-    }).then(response => {
-      console.log("Consulta de CEP solicitada a API com sucesso!");
-      res.send(response.data);
+    if(validaCep.test(cep)){
+      const url = "http://viacep.com.br/ws/"+cep+"/json/";
+      console.log("Solicitando endereço para CEP: "+cep)
     
-    }).catch(e => {
-      console.log("Consulta de CEP solicitada a API com erro inesperado...");
-      console.log(e);
-      res.send(e);
+      axios({
+        method: 'get',
+        url: url,
 
-    })
+      }).then(response => {
+        console.log("Consulta de CEP solicitada a API com sucesso!");
+        if(response.data.error) {
+          console.log("CEP não encontrado...");
+          res.status(404).send("Not found!");
+        }
+
+        else res.send(response.data);
+        
+      }).catch(e => {
+        console.log("Consulta de CEP solicitada a API com erro inesperado...");
+        res.status(500).send({error: e});
+    
+      })
+    } else {
+      console.log("Formato de cep inválido!");
+      res.status(400).send("Formato invalido");
+
+    }
     
   },
 }
