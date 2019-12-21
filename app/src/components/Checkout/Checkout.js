90dd9bde19373447352ea0b9da3bcabb08e2bfa2
GustavoM7 <gustavosmarquesf@gmail.com>
2019-12-21 20:40:09 -0300

    substituindo alerts

--- app/src/components/Checkout/Checkout.js
+++ app/src/components/Checkout/Checkout.js
@@ -2,164 +2,201 @@ import React, {Component} from "react";
 import Produtos from "../../service/Dados";
 import api from "../../service/Api";
 import {Link} from "react-router-dom";
+import Popup from '../Popup/Popup';
 
 class Checkout extends Component{
-    state = {
-      carrinho: [],
+  state = {
+    carrinho: [],
 
-      //Dados do comprador:
-      name: "Gustavo Marques",
-      email: "c13823858083569199133@sandbox.pagseguro.com.br",
-      phoneAreaCode: "51",
-      phoneNumber: "88888888",
+    //Dados do comprador:
+    name: "Gustavo Marques",
+    email: "c13823858083569199133@sandbox.pagseguro.com.br",
+    phoneAreaCode: "51",
+    phoneNumber: "88888888",
 
-      //Dados do frete:
-      type: 1,
-      street: "Rua teste",
-      number: "41",
-      complement: "Complemento teste",
-      district: "Bairro teste",
-      postalCode: "01452002",
-      city: "São Paulo",
-      state: "SP",
-      country: "BRA",
-    }
+    //Dados do frete:
+    type: 1,
+    street: "Rua teste",
+    number: "41",
+    complement: "Complemento teste",
+    district: "Bairro teste",
+    postalCode: "01452002",
+    city: "São Paulo",
+    state: "SP",
+    country: "BRA",
 
-    handleInput = (e) =>{
-      this.setState({
-          [e.target.name]: e.target.value
-      })
-    }
+    //Popup para retornos
+    popText: "",
+    popWarning: false,
+    popVisibility: false,
+    popPermanent: false,
 
-    submit = () => {
-      const st = this.state;
+  }
 
-      if(st.carrinho.length === 0) alert("O Carrinho está vazio...");
-      else if(st.name &&
-        st.email &&
-        st.phoneAreaCode &&
-        st.phoneNumber &&
-        st.street &&
-        st.number &&
-        st.district &&
-        st.postalCode &&
-        st.city &&
-        st.state &&
-        st.country){
+  callPopup = (text, warning, visibility, permanent) =>{
+    this.setState({
+      popText: text,
+      popWarning: warning,
+      popVisibility: visibility,
+      popPermanent: permanent,
+    })
+  }
 
-          let ref = Math.random() * 10000;
+  closePopup = () =>{
+    this.setState({
+      popText: "",
+      popWarning: false,
+      popVisibility: false,
+      popPermanent: false,
+    })
+  }
 
-          let comprador = {
-            name: st.name,
-            email: st.email,
-            phoneAreaCode: st.phoneAreaCode,
-            phoneNumber: st.phoneNumber,
-            ref: ref,
-          }
+  handleInput = (e) =>{
+    this.setState({
+        [e.target.name]: e.target.value
+    })
+  }
 
-          let frete = {
-            type: 1,
-            street: st.street,
-            number: st.number,
-            complement: st.complement,
-            district: st.district,
-            postalCode: st.postalCode,
-            city: st.city,
-            state: st.state,
-            country: st.country,
-          }
+  submit = () => {
+    const st = this.state;
 
-          console.log("processando...")
+    if(st.carrinho.length === 0) this.callPopup("O Carrinho está vazio...", true, true, false);
+    else if(st.name &&
+      st.email &&
+      st.phoneAreaCode &&
+      st.phoneNumber &&
+      st.street &&
+      st.number &&
+      st.district &&
+      st.postalCode &&
+      st.city &&
+      st.state &&
+      st.country){
 
-          let req = {carrinho: Produtos, comprador: comprador, frete: frete};
+        let ref = Math.random() * 10000;
 
-          api.post("/pagseguro", req).then((res) => {
-            console.log(res.data);
-            let redirectKey = res.data.checkout.code._text;
-            alert("Você será redirecionado para finalizar o pagamento!");
-            window.location.href = 'https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html?code='+redirectKey;
-          }).catch((e) => {console.log(e)});
+        let comprador = {
+          name: st.name,
+          email: st.email,
+          phoneAreaCode: st.phoneAreaCode,
+          phoneNumber: st.phoneNumber,
+          ref: ref,
         }
 
-        else alert("Preencha todos os dados!");
-    }
+        let frete = {
+          type: 1,
+          street: st.street,
+          number: st.number,
+          complement: st.complement,
+          district: st.district,
+          postalCode: st.postalCode,
+          city: st.city,
+          state: st.state,
+          country: st.country,
+        }
 
-    componentDidMount(){
-      this.setState({carrinho: Produtos});
-    }
+        this.callPopup("Processando dados...", false, true, true);
 
-    render(){
-      return(
-        <div className="App">
-          <header className="App-header Half-screen">
-              <h1>FORMULÁRIO DE CHECKOUT</h1>
-              <p>Preencha abaixo para simular uma compra com <em>PagSeguro</em></p>
-              <Link to="/home" className="App-link">Tela inicial</Link>
-          </header>
-          <form>
-            <h3>DADOS DO COMPRADOR</h3>
-            <section>
-            <div className="form-row">
-              <label>NOME:</label>
-              <input name="name" value={this.state.name} onChange={this.handleInput}/>
-            </div>
+        let req = {carrinho: Produtos, comprador: comprador, frete: frete};
 
-            <div className="form-row">
-              <label>EMAIL:</label>
-              <input name="email" value={this.state.email} onChange={this.handleInput}/>
-            </div>
+        api.post("/pagseguro", req).then((res) => {
+          this.callPopup("Redirecionando...", false, true, true);
+          let redirectKey = res.data.checkout.code._text;
+          window.location.href = 'https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html?code='+redirectKey;
 
-            <div className="form-row">
-              <label>DDD:</label>
-              <input className="short-input" name="phoneAreaCode" value={this.state.phoneAreaCode} onChange={this.handleInput}/>
+        }).catch((e) => {
+          console.log(e);
+          this.callPopup("Erro inesperado!", true, true, false);
 
-              <label>TELEFONE:</label>
-              <input name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleInput}/>
-            </div>
-            </section>
+        });
+      }
 
-            <hr/>
+      else this.callPopup("Preencha todos os dados!", true, true, false);
+  }
 
-            <h3>DADOS DO FRETE</h3>
-            <section>
-            <div className="form-row">
-              <label>RUA:</label>
-              <input name="street" value={this.state.street} onChange={this.handleInput}/>
+  componentDidMount(){
+    this.setState({carrinho: Produtos});
+  }
 
-              <label>NÚMERO:</label>
-              <input className="short-input" name="number" value={this.state.number} onChange={this.handleInput}/>
+  render(){
+    return(
+      <div className="App">
+        <header className="App-header Half-screen">
+            <h1>FORMULÁRIO DE CHECKOUT</h1>
+            <p>Preencha abaixo para simular uma compra com <em>PagSeguro</em></p>
+            <Link to="/home" className="App-link">Tela inicial</Link>
+        </header>
+        <form>
+          <h3>DADOS DO COMPRADOR</h3>
+          <section>
+          <div className="form-row">
+            <label>NOME:</label>
+            <input name="name" value={this.state.name} onChange={this.handleInput}/>
+          </div>
 
-              <label>COMPLEMENTO:</label>
-              <input name="complement" value={this.state.complement} onChange={this.handleInput}/>
-            </div>
+          <div className="form-row">
+            <label>EMAIL:</label>
+            <input name="email" value={this.state.email} onChange={this.handleInput}/>
+          </div>
 
-            <div className="form-row">
-              <label>BAIRRO:</label>
-              <input name="district" value={this.state.district} onChange={this.handleInput}/>
+          <div className="form-row">
+            <label>DDD:</label>
+            <input className="short-input" name="phoneAreaCode" value={this.state.phoneAreaCode} onChange={this.handleInput}/>
 
-              <label>CEP:</label>
-              <input className="short-input" name="postalCode" value={this.state.postalCode} onChange={this.handleInput}/>
-            </div>
-            
-            <div className="form-row">
-              <label>CIDADE:</label>
-              <input name="city" value={this.state.city} onChange={this.handleInput}/>
+            <label>TELEFONE:</label>
+            <input name="phoneNumber" value={this.state.phoneNumber} onChange={this.handleInput}/>
+          </div>
+          </section>
 
-              <label>ESTADO:</label>
-              <input name="state" value={this.state.state} onChange={this.handleInput}/>
+          <hr/>
 
-              <label>PAÍS:</label>
-              <input name="country" value={this.state.country} onChange={this.handleInput}/>
-            </div>
+          <h3>DADOS DO FRETE</h3>
+          <section>
+          <div className="form-row">
+            <label>RUA:</label>
+            <input name="street" value={this.state.street} onChange={this.handleInput}/>
 
-            </section>
-            <hr/>
-            <button type="button" onClick={() => this.submit()}>CONFIRMAR</button>
+            <label>NÚMERO:</label>
+            <input className="short-input" name="number" value={this.state.number} onChange={this.handleInput}/>
 
-          </form>
-        </div>
-      );
-    }
+            <label>COMPLEMENTO:</label>
+            <input name="complement" value={this.state.complement} onChange={this.handleInput}/>
+          </div>
+
+          <div className="form-row">
+            <label>BAIRRO:</label>
+            <input name="district" value={this.state.district} onChange={this.handleInput}/>
+
+            <label>CEP:</label>
+            <input className="short-input" name="postalCode" value={this.state.postalCode} onChange={this.handleInput}/>
+          </div>
+          
+          <div className="form-row">
+            <label>CIDADE:</label>
+            <input name="city" value={this.state.city} onChange={this.handleInput}/>
+
+            <label>ESTADO:</label>
+            <input name="state" value={this.state.state} onChange={this.handleInput}/>
+
+            <label>PAÍS:</label>
+            <input name="country" value={this.state.country} onChange={this.handleInput}/>
+          </div>
+
+          </section>
+          <hr/>
+          <button type="button" onClick={() => this.submit()}>CONFIRMAR</button>
+
+        </form>
+        <Popup 
+          warning={this.state.popWarning}
+          visible={this.state.popVisibility}
+          permanent={this.state.popPermanent}
+          onClose={this.closePopup}>
+          {this.state.popText}
+        </Popup>
+      </div>
+    );
+  }
 }
 
 export default Checkout;
